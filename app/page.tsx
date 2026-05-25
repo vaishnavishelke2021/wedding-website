0c7dec5e66fd3f912de0e9642dc9303aa82f4380
Vaishnavi Shelke <vaishnavishelke7777@gmail.com>
2026-05-25 14:46:55 +0530

    smooth scroll added

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
+'use strict'; 
+'use client';
 
+import { ReactLenis } from '@studio-freight/react-lenis';
 import Entry from '@/components/Entry';
-import Events from '@/components/Events';
 import Weddingnav from '@/components/Weddingnav';
-import Engagementsection from '@/components/Engagementsection';
 import Ceremonies from '@/components/Ceremonies';
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <Entry />
-      {/* <Events /> */}
-      <Weddingnav />
-      {/* <Engagementsection/> */}
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: false }}>
+      <main className="w-full h-full">
+        <Entry />
+        {/* <Events /> */}
+        <Weddingnav />
+        {/* <Engagementsection/> */}
         <Ceremonies />
-    </main>
+      </main>
+    </ReactLenis>
   );
 }
