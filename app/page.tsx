4f645fb0b8b0f6872f0ca51c51c4ee4142993505
Vaishnavi Shelke <vaishnavishelke7777@gmail.com>
2026-05-25 14:50:45 +0530

    installed lenis

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
-'use strict'; 
 'use client';
 
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from 'lenis/react';
 import Entry from '@/components/Entry';
 import Weddingnav from '@/components/Weddingnav';
 import Ceremonies from '@/components/Ceremonies';
 
 export default function Home() {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: false }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
       <main className="w-full h-full">
         <Entry />
-        {/* <Events /> */}
         <Weddingnav />
-        {/* <Engagementsection/> */}
         <Ceremonies />
       </main>
     </ReactLenis>
